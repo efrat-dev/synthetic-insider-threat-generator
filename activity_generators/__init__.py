1995469c2f6a9b96a9b1e391c4ee8eef372fb62a
efchbd1013 <efchbd1013@gmail.com>
2025-07-06 12:53:10 +0300

    feat(activity_generators): Update imports for PrintActivityGenerator

--- activity_generators/__init__.py
+++ activity_generators/__init__.py
@@ -1,8 +1,8 @@
-from print_activity_generator import PrintActivityGenerator
-from burn_activity_generator import BurnActivityGenerator
-from travel_activity_generator import TravelActivityGenerator
-from access_activity_generator import AccessActivityGenerator
-from risk_indicators import RiskIndicatorGenerator
+from .print_activity_generator import PrintActivityGenerator
+from .burn_activity_generator import BurnActivityGenerator
+from .travel_activity_generator import TravelActivityGenerator
+from .access_activity_generator import AccessActivityGenerator
+from .risk_indicators import RiskIndicatorGenerator
 
 __all__ = [
     'PrintActivityGenerator',
